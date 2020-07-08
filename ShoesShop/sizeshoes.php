90b5dc7599021f0ef678b905cc98fd5b6f410e77
thanh sarah <thanhnguyen1407ba@gmail.com>
2020-07-08 23:53:38 +0700

    lieu lam cho bo

---
+++ ShoesShop/sizeshoes.php
@@ -0,0 +1,104 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Vian Shop - <?php echo htmlentities($row1['name']);?></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style-index.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" type="image/png" href="image-page/logo2.PNG">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<?php 
+    include "includes/top-header.php";
+    include 'includes/header.php';
+    ?>
+    <div class="container">
+        <div class="name-page">
+            <h6><a href="index.php">Trang chủ</a><span> / Danh sách yêu thích</span></h6>
+        </div>
+        <br>
+    <h1>CÁCH ĐO SIZE GIÀY VÀ BẢNG QUY ĐỔI SIZE GIÀY THỂ THAO</h1>
+    <h2>Phần 1: CÁCH ĐO BÀN CHÂN CHỌN SIZE GIÀY THEO SIZE US</h2>
+    
+    <table>
+        <tr>
+            <td style="vertical-align: top;">
+            <h3>Bước 1: Chuẩn bị để đo size chân theo bảng size</h3>
+            <p style="font-size: larger">– Lấy một tờ giấy đủ độ dài bàn chân, một cây thước, một cây viết (bút)<br></p>
+
+            <p style ="font-size:larger">– Mang (đi) tất (vớ) mà bạn dự định đi với đôi giày bạn cần mua.<br></p>
+
+            <p style ="font-size:larger">– Khuyến nghị nên đo vào buổi chiều để có chiều dài dài nhất.</p>
+            </td>
+            <td>
+            <img src="./image-page/chon-do-size-giay-da-bong-chuan.jpg" height="200px" width="auto">
+            </td>
+        </tr>
+        <tr>
+            <td style="vertical-align: top;">
+            <h3>Bước 2: Cách đo độ dài bân chân để tính size giày</h3>
+            <p style="font-size: larger">– Đặt tờ giấy xuống sàn, sát vào góc tường mà bạn có thể đứng dựa vào được.<br></p>
+
+            <p style ="font-size:larger">– Dùng bút (viết) vạch vào mũi chân dài nhất (nên đo 2 chân để lấy chiều dài nhất.<br></p>
+
+            <p style ="font-size:larger">– Sau đó đo lại vuông gốc từ mép dây đến vạch kẻ đầu mủi chân (không đo chéo sẽ sai số)</p>
+            <p style="font-size: larger">– Lúc này ta có con số dài nhất một trong hai bàn chân.<br></p>
+            </td>
+            <td>
+            <img src="./image-page/cach-dat-ban-chan.jpg" height="200px" width="auto">
+            </td>
+        </tr>
+        <tr>
+        <td style="vertical-align: top;">
+            <h3>Bước 3: Đo với bàn chân có bề ngang to hoặc mu bàn chân dày</h3>
+            <p style="font-size: larger">– Bạn chọn vị trí gần đầu mủi chân có bề ngang to hoặc mu bàn chân dày nhất.<br></p>
+
+            <p style ="font-size:larger">– Dùng thước dây vòng 1 vòng chân (M như hình) tạo thành chu vi.<br></p>
+
+            <p style ="font-size:larger">– Nếu không có thước dây có thể dùng dây đo sau đó đo lại bằng thước cứng.</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+            <h3>Bước 4: Lấy số đo để tính ra size giày chuẩn.</h3>
+            <p style="font-size: larger">– Bạn so sánh số đo ở bước 2 và số đo bước 3.<br></p>
+
+            <p style ="font-size:larger">– Lấy con số lớn nhất để so với bảng size phía dưới ở cột “Centimet”<br></p>
+            </td>
+            <tr>
+                <td>
+                <img src="./image-page/quy-doi-size-giay.png" height="550px" width="auto">
+            
+                </td>
+                <td>
+                <img src="./image-page/quy-doi-size-giay-1.png"  height="550px" width="auto">
+                </td>
+            </tr>
+        </tr>
+    </table>
+    <h2>Phần 2: BẢNG QUY ĐỔI CHI TIẾT THEO HÃNG</h2>
+    <p style="font-size:larger">Do có một số hãng có quy đổi khác giữ US – Euro. Nên bạn có thể tham khảo thêm bảng size từng hãng.</p>
+    <img src="./image-page/Annotation 2020-07-08 233919.png" height="750px" width="750px">
+    <img src="./image-page/Annotation 2020-07-08 233959.png" height="750px" width="750px">
+    <img src="./image-page/Annotation 2020-07-08 234024.png" height="750px" width="750px">
+<?php include 'includes/FOOTER.php';?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+</html>
